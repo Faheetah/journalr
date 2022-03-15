1958a024c0ff36c628f4494acd322ce00addaa75
Faheetah <Faheetah@users.noreply.github.com>
2022-03-15 11:31:32 -0500

    Add tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :journalr, JournalrWeb.Endpoint,
   secret_key_base: "7rIZl6WaY9+Ki7k06O2vp9Zfe7b1vuSmgHYSoKKDrDO4jVLM3m8sbo4kTW1tIKAm",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
