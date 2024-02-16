0505286bd2975e36bf1fad4440acb52d4139bee3
Faheetah <Faheetah@users.noreply.github.com>
2024-02-16 10:20:03 -0600

    Ability to build as a container image

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :swoosh, :api_client, false
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.14.0",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
